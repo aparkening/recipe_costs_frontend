8260f19215b71b9d468e7f20226a66fa9a3c9583
aparkening <aaron.parkening@gmail.com>
2019-12-17 18:40:55 -0500

    Add basic showHTML for recipe with many ingredients.

--- src/components/recipe.js
+++ src/components/recipe.js
@@ -1,23 +1,86 @@
 class Recipe{
   constructor(recipeObj){
-    const { recipe, recipeIngredients } = recipeObj
+    const { recipe, ingredients } = recipeObj
 
     this.id = recipe.id
     this.name = recipe.name
     this.servings = recipe.servings
+    this.totalCost = recipe.total_cost
+    this.costPerServing = recipe.cost_per_serving
 
-    // this.ingredients = recipeIngredients.map(ingredient => new Ingredient(ingredient))
-
+    this.ingredients = ingredients.map(ing => new Ingredient(ing.ingredient))
   }
 
   get showHTML(){
-    return (`
-      <h2>${this.name}</h2>
-      <h4>${this.servings}</h4>
+    let html = `
+      <h1>${this.name}</h1>
+      <h2>Total Cost: $${this.totalCost}</h2>
+    `
 
-    `)
+    if (this.servings !== null){
+      html = html + `<h4>Serves ${this.servings} | Cost per serving: $${this.servings}</h4>`
+    }
+
+    const ingTableTop = `
+    <table class="table table-striped">
+      <thead class="thead-dark">
+        <tr>
+          <th>Cost</th>
+          <th>Name</th>
+          <th>Amount</th>
+          <th>Amount Unit</th>
+        </tr>
+      </thead>
+      <tbody>
+    `
+
+    const ingTableBottom = `
+      </tbody>  
+    </table>
+    `
+
+console.log(this.ingredients)
+
+    const ingHTML = this.ingredients.map(ing => {
+      return (`
+      <tr>
+        <td>$${ing.cost}</td>
+        <th scope="row">${ing.name}</th>
+        <td>Amount</td>
+        <td>Amount Unit</td>
+      </tr>  
+      `)
+    }).join('')
+    // const ingHTML = 'testing'
+
+    // html = html + this.ingredients
+    html = html + ingTableTop + ingHTML + ingTableBottom
+
+    return html
   }
-}
 
-{/* <p>Ingredients: \n${this.recipeIngredients ? this.recipeIngredients : "None"}</p> */}
-{/* <button data-id=${this.id}  id="edit-recipe">Edit</button> */}
+  // Render recipes table
+  get showRecipes(){
+    return (`
+      <table class="table table-striped">
+        <thead class="thead-dark">
+          <tr>
+            <th>Name</th>
+            <th>Cost</th>
+            <th>Links</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row"><a href="/users/10/recipes/15">Cinnamon Raisin Bread</a></th>
+            <td>$3.05
+              &nbsp;|&nbsp; per serving: $0.38
+            </td>
+            <td><small><a class="text-muted" href="/users/10/recipes/15/edit">Edit</a> | <a class="text-muted" data-confirm="Are you sure?" rel="nofollow" data-method="delete" href="/users/10/recipes/15">Delete</a></small></td>
+          </tr>   
+        </tbody>  
+      </table>`)
+
+  }
+
+}
