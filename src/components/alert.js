36613880cd0e80577d463b7a7d396d8389baebe0
aparkening <aaron.parkening@gmail.com>
2019-12-17 14:14:07 -0500

    Add destructured assignment for alert.

--- src/components/alert.js
+++ src/components/alert.js
@@ -4,23 +4,26 @@ class Alert{
       this.container = container
   }
 
+  // Clear alert after timeout
   stopAlert(){
       clearTimeout(this.timeout)
       this.container.innerHTML = ''
   }
 
-  // Render alert in bootstrap styling
-  render(msg, type, timeout){
-      const html = `
-      <div class="alert alert-${type}" role="alert">
-          ${msg}
-      </div>
-      `
-      this.container.innerHTML = html
-      
-      this.timeout = setTimeout(() => {
-          this.container.innerHTML = ''
-      }, timeout)
-  }
+    // Render alert in bootstrap styling
+    render(alertObj, timeout){
+        const { type, msg } = alertObj
+        const html = `
+        <div class="alert alert-${type}" role="alert">
+            ${msg}
+        </div>
+        `
+        this.container.innerHTML = html
+        
+        // Display alert for time set in timeout
+        this.timeout = setTimeout(() => {
+            this.container.innerHTML = ''
+        }, timeout)
+    }
 
 }
