206ab0b88f06a095a6a0de9af364010e6de5d5d7
aparkening <aaron.parkening@gmail.com>
2019-12-17 17:31:38 -0500

    Add initial ingredient tr render method, table method, and showIngredients.

--- src/components/ingredient.js
+++ src/components/ingredient.js
@@ -1,18 +1,58 @@
 class Ingredient{
-  constructor(ingredient){
-      const { id, name, cost, cost_size, cost_unit } = ingredient
-      this.id = id
-      this.name = name
-      this.cost = cost
-      this.cost_size = cost_size
-      this.cost_unit = cost_unit
-      // this.recipes = recipes.map(r => new Recipe(r))
+  constructor(ingredientObj){
+    const { id, name, cost, cost_size, cost_unit } = ingredientObj
+    this.id = id
+    this.name = name
+    this.cost = cost
+    this.costSize = cost_size
+    this.costUnit = cost_unit
+    // this.recipes = recipes.map(r => new Recipe(r))
   }
 
-  get showHTML(){
-      return (`
-      <h2>${this.name}</h2>
-      <h4>${this.cost}</h4>
-      `)
+  // Render tr for each ingredient
+  renderTr(){
+    return (`
+    <tr>
+      <th><small><a class="text-muted" href="#">Edit</a> | <a class="text-muted" data-confirm="Are you sure?" rel="nofollow" data-method="delete" href="#">Delete</a></small></th>
+      <th scope="row">${this.name}</th>
+      <td>$ ${this.cost}</td>
+      <td>${this.costSize}</td>
+      <td>${this.costUnit}</td>
+    </tr> 
+    `)
+  } 
+
+  // Show full table of ingredients
+  showTable(){
+    const tHead = `
+    <table class="table table-striped">
+      <thead class="thead-dark">
+        <tr>
+          <th>Links</th>
+          <th>Name</th>
+          <th>Cost</th>
+          <th>Cost Size</th>
+          <th>Cost Unit</th>
+        </tr>
+      </thead>
+      <tbody>
+    `
+    const tFoot = `
+      </tbody>  
+    </table>`
+  }
+    
+  get showIngredients(){
+    let html = 'ingredients'
+    // let html = `
+    // <p>${this.name}, </p>
+    // <h2>Total Cost: $${this.totalCost}</h2>
+    // `
+
+    // if (this.servings !== null){
+    //     html = html + `<h4>Serves ${this.servings} | Cost per serving: $${this.servings}</h4>`
+    // }
+
+    return html
   }
 }
