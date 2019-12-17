b6b81ac1eec8dac5a7a37f8e9f5f5cb434aa0acc
aparkening <aaron.parkening@gmail.com>
2019-12-17 17:32:19 -0500

    Update fetch and render method with new ingredient object.

--- src/pageManagers/ingredientManager.js
+++ src/pageManagers/ingredientManager.js
@@ -28,6 +28,31 @@ class IngredientPage extends PageManager{
   //     this.redirect('ingredients')
   // }
 
+  async fetchAndRenderPageResources(){
+    try{
+        // console.log("fetching recipe")
+        const ingredientObj = await this.adapter.getIngredients()
+        // console.log(ingredientObj)
+
+        // this.ingredients = ingredientObj.map(ing => new Ingredient(ing))
+        // console.log(this.recipe)
+        console.log(ingredientObj.ingredients)
+        
+        // const newIngredients = ingredientObj.ingredients.forEach(ing => new Ingredient(ing))
+
+        // console.log(ingredients)
+
+        // this.renderIngredients()
+    }catch(err){
+        // this.handleError(err)
+        console.log(err)
+    }
+  }
+
+  renderIngredients(){
+    this.container.innerHTML = this.ingredient.showIngredients
+  }
+
   // Render initial html
   get staticHTML(){
     return (`
