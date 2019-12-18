f92a391b3581906c589abf3d5db5b73e3e0fa050
aparkening <aaron.parkening@gmail.com>
2019-12-18 07:05:07 -0500

    Add comments and update indents.

--- src/pageManagers/pageManager.js
+++ src/pageManagers/pageManager.js
@@ -10,12 +10,13 @@ class PageManager{
 
   // Display errors
   handleError(err){
-      // if(err.type === "Authorizaiton Error"){
-      //     this.handleAlert(err.msg)
-      //     this.redirect('welcome')
-      // }else{
-          this.handleAlert(err)
-      // }
+    // Activate upon user auth
+    // if(err.type === "Authorizaiton Error"){
+    //     this.handleAlert(err.msg)
+    //     this.redirect('welcome')
+    // }else{
+      this.handleAlert(err)
+    // }
   }
 
   // Display initial static html, listen for changes, then fetch and render changes
