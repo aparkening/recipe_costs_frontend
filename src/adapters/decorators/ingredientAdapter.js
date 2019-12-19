814c7c0b6115b886cb067a43304fe6b0bfc372e2
aparkening <aaron.parkening@gmail.com>
2019-12-19 14:34:09 -0500

    Add get single record method. Move get single and multiple to top. Indent other methods.

--- src/adapters/decorators/ingredientAdapter.js
+++ src/adapters/decorators/ingredientAdapter.js
@@ -1,16 +1,30 @@
 class IngredientAdapter{
 
   constructor(baseAdapter){
-      this.baseAdapter = baseAdapter
-      this.baseURL = this.baseAdapter.baseURL
+    this.baseAdapter = baseAdapter
+    this.baseURL = this.baseAdapter.baseURL
   }
 
-  // get token(){
-  //     return this.baseAdapter.token
-  // }
-
   get headers(){
-      return this.baseAdapter.headers
+    return this.baseAdapter.headers
+  }
+
+  // Get all records
+  async getIngredients(){
+    const res = await fetch(`${this.baseURL}/ingredients`, {
+      headers: this.headers
+    })
+    await this.baseAdapter.checkStatus(res)
+    return await res.json()
+  }
+
+  // Get single record
+  async getIngredient(id){
+    const res = await fetch(`${this.baseURL}/ingredients/${id}`, {
+      headers: this.headers
+    })
+    await this.baseAdapter.checkStatus(res)
+    return await res.json()
   }
 
   // Create record
@@ -34,33 +48,22 @@ class IngredientAdapter{
     return await res.json()
   }
 
-
   // Update record
   async updateIngredient(params){
-
-      const { name, cost, cost_size, cost_unit, id} = params
-      const url = `${this.baseURL}/ingredients/${id}`
-      const body = {
-          ingredient: {
-            name, 
-            cost,
-            cost_size,
-            cost_unit
-          }
-      }
-      const res = await fetch(url, {
-          method: 'PATCH',
-          headers: this.headers,
-          body: JSON.stringify(body)
-      })
-      await this.baseAdapter.checkStatus(res)
-      return await res.json()
-  }
-
-  // Fetch all records
-  async getIngredients(){
-    const res = await fetch(`${this.baseURL}/ingredients`, {
-      headers: this.headers
+    const { name, cost, cost_size, cost_unit, id} = params
+    const url = `${this.baseURL}/ingredients/${id}`
+    const body = {
+        ingredient: {
+          name, 
+          cost,
+          cost_size,
+          cost_unit
+        }
+    }
+    const res = await fetch(url, {
+        method: 'PATCH',
+        headers: this.headers,
+        body: JSON.stringify(body)
     })
     await this.baseAdapter.checkStatus(res)
     return await res.json()
