321f363bb0f8ac0f9f7938fc719b5c89d9939a4c
aparkening <aaron.parkening@gmail.com>
2019-12-20 08:42:28 -0500

    Add render and bindings to handleCancelClick().

--- src/pageManagers/ingredientManager.js
+++ src/pageManagers/ingredientManager.js
@@ -71,7 +71,8 @@ class IngredientPage extends PageManager{
   // Handel form cancel click
   handleCancelClick(e){
     e.preventDefault()
-
+    this.newContainer.innerHTML = this.renderNewBtn()
+    this.indexBindingsAndEventListeners()
   }
 
   // Handle form submit
