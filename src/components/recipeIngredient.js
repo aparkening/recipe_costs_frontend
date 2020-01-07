4898aff6e861380b6bdca2940b3e8e894d43ad4d
aparkening <aaron.parkening@gmail.com>
2020-01-07 11:42:47 -0500

    Add showEditIng() to display existing ingredients in recipe form.

--- src/components/recipeIngredient.js
+++ src/components/recipeIngredient.js
@@ -22,4 +22,37 @@ class RecipeIngredient{
     `)
   }
 
+  // Render existing ingredients in recipe form
+  showEditIng(units){
+    return (`
+    <div class="form-row form-group form-ingredient">
+      <div class="col-3">
+        ${this.name}
+      </div>
+      <div class="col-1">
+        <input placeholder="1" type="text" name="ingredient_amount" class="ingredient_amount form-control" required="required" value="${this.amount}">
+      </div>
+
+      <!-- How to select existing amountUnit? -->
+      <div class="col-2">
+        <select name="ingredient_unit" class="ingredient_unit custom-select">${units.map(u => this.renderUnitOptions(u)).join('')}
+        </select>
+      </div>
+      <div class="col pt-2 small">
+        <a href="#" class="text-muted delete-existing">Delete</a>
+      </div>
+
+      <input type="hidden" value="${this.id}" name="ingredient_id" id="" class="ingredient_id">
+    </div><!-- / form-ingredient -->
+    `)
+  }
+
+  // Render units as select list
+  renderUnitOptions(unit){
+    // console.log(this)
+    return (`
+    <option value="${unit}">${unit}</option>
+    `)
+  }
+
 }
