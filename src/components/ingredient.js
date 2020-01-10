17e0a748f46cd4a166ea18c8ed016dea44b302d8
aparkening <aaron.parkening@gmail.com>
2020-01-10 16:40:18 -0500

    Add deleteField() to delete appropriate form fields.

--- src/components/ingredient.js
+++ src/components/ingredient.js
@@ -46,6 +46,24 @@ class Ingredient{
     `)
   }
 
+  // Delete appropriate ingredient form field 
+  static deleteField(e) {
+    // If delete on new ingredient, remove node
+    if (e.target.classList.contains('delete')) { e.target.parentNode.parentNode.remove() }
+
+    // If delete on existing ingredient, hide node and set hidden input
+    if (e.target.classList.contains('delete-existing')){
+      let deleteIng = document.createElement('input')
+      deleteIng.type = "hidden"
+      deleteIng.value = 1
+      deleteIng.name = "destroy"
+      e.target.parentNode.parentNode.appendChild(deleteIng)
+
+      //e.target.parentNode.parentNode.remove()
+      e.target.parentNode.parentNode.classList.add("hidden")
+    }
+  }
+
     
   // get showIngredients(){
   //   let html = 'ingredients'
