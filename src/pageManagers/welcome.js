dbf96e9e67dc891ac25901126e0b09a6f9702ef1
aparkening <aaron.parkening@gmail.com>
2020-04-30 14:22:09 -0400

    Remove some formality from description.

--- src/pageManagers/welcome.js
+++ src/pageManagers/welcome.js
@@ -28,7 +28,7 @@ class WelcomePage extends PageManager{
       return (`
         <div class="jumbotron bg-white">
           <h1 class="display-4">Price recipes easily and quickly!</h1>
-          <p class="lead">Recipe Costs helps you to record your recipes and ingredient costs in one location.</p>
+          <p class="lead">Recipe Costs helps you record your recipes and ingredient costs in one place.</p>
           <hr class="my-4">
           <a class="btn btn-primary btn-lg" href="#" role="button" id="recipes">All Recipes</a> &nbsp; or &nbsp;
           <a class="btn btn-primary btn-lg" href="#" role="button" id="ingredients">All Ingredients</a>
